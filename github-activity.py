e1184fc378029a5671f8ac6638e4616aad4aad9b
pinajr <122037160+pinajr@users.noreply.github.com>
2026-09-11 11:47:49 -0300

    feat: CLI argument and GitHub username validation
    * Add `parser_argument()` to validate CLI argument count and format.
    * Add `validate_username()` to validate GitHub usernames using GitHub's
    username rules.
    * Prevent invalid input from reaching the application logic.

--- github-activity.py
+++ github-activity.py
@@ -3,37 +3,29 @@ import json
 import re
 from urllib.request import urlopen
 
-# Verify if the argument and username are valid to test
-def verify_argument():
-    try:
-        valid_user = sys.argv[1]
-
-        # Verify the quantity of arguments insert in the CLI
-        if len(sys.argv) > 2 or len(sys.argv) == 1:
-            print("Please, insert a right quantity of argument.")
-            sys.exit(1)
-        else:
-            # Laws to has a GitHub usernames
-            if not re.match(r"^[a-zA-Z0-9][a-zA-Z0-9-]{0,37}[a-zA-Z0-9]$", valid_user):
-                if len(valid_user) == 1 and valid_user.isalnum():
-                    print("Please, insert one more character.")
-                    sys.exit(1)
-                print("Please, insert a valid user.")
-                sys.exit(1)
-
-            if "--" in valid_user:
-                print("The '--' is not valid. Please insert a valid username.")
-                sys.exit(1)
-
-            return valid_user
-    # If don't have any argument to verify
+def parse_argument():
+    # Retrieve the raw CLI data to valid it and prevents an IndexError.
+    try:        
+        if len(sys.argv) > 2:
+            print("The correct command is 'github-actitivy.py <username>'"
+            "\nPlease try again.")
+            return None
+        return sys.argv[1]
     except IndexError:
-        print("Please, insert a username to verify the activity.")
+        print("Please insert a <username> to verify the activity.")
+        return None
+    
+
+def validate_username(valid_user):
+    # Checks if the username complies with GitHub's regex rules.
+    if not re.fullmatch(r"[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*", valid_user):
+        print("Please, insert a valid user.")
         sys.exit(1)
+    return valid_user
 
 
-# Make a resquest to the GitHub API to get the user's events
 def make_request(username):
+    # Make a resquest to the GitHub API to get the user's events.
     response = urlopen(f'https://api.github.com/users/{username}/events')
     data = json.loads(response.read().decode('utf-8'))
 
@@ -47,9 +39,7 @@ def make_request(username):
 
 
 def main():
-    username = verify_argument()
-
-    make_request(username)
+    parse_argument()
 
 
 if __name__ == "__main__":
