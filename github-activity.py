d5af8d640bbc211a6a26f28fa12dd396c06bddb0
pinajr <122037160+pinajr@users.noreply.github.com>
2026-09-10 23:45:59 -0300

    Feature to valid arguments and username

--- github-activity.py
+++ github-activity.py
@@ -1,6 +1,36 @@
 import sys
 import json
+import re
 from urllib.request import urlopen
+
+# Verify if the argument and username are valid to test
+def verify_argument():
+    try:
+        valid_user = sys.argv[1]
+
+        # Verify the quantity of arguments insert in the CLI
+        if len(sys.argv) > 2 or len(sys.argv) == 1:
+            print("Please, insert a right quantity of argument.")
+            sys.exit(1)
+        else:
+            # Laws to has a GitHub usernames
+            if not re.match(r"^[a-zA-Z0-9][a-zA-Z0-9-]{0,37}[a-zA-Z0-9]$", valid_user):
+                if len(valid_user) == 1 and valid_user.isalnum():
+                    print("Please, insert one more character.")
+                    sys.exit(1)
+                print("Please, insert a valid user.")
+                sys.exit(1)
+
+            if "--" in valid_user:
+                print("The '--' is not valid. Please insert a valid username.")
+                sys.exit(1)
+
+            return valid_user
+    # If don't have any argument to verify
+    except IndexError:
+        print("Please, insert a username to verify the activity.")
+        sys.exit(1)
+
 
 # Make a resquest to the GitHub API to get the user's events
 def make_request(username):
@@ -14,10 +44,11 @@ def make_request(username):
             count += 1
     print(f"Count of Events: {count}")
     '''
-    
+
 
 def main():
-    username = sys.argv[1]
+    username = verify_argument()
+
     make_request(username)
 
 
